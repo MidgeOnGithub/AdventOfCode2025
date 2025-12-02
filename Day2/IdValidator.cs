077a58184a8cfec8f1c8fb65fc97e5ca4027ce50
Cody <midge@midge.me>
2025-12-02 14:10:24 -0500

    Completed Day 2 Part 2.

--- Day2/IdValidator.cs
+++ Day2/IdValidator.cs
@@ -12,11 +12,42 @@ public class IdValidator
         {
             var digits = i.ToString();
             int length = digits.Length;
-            if (length % 2 != 0) // Odd-length numbers cannot have a twice-repeated pattern as defined by the puzzle.
+            if (length < 2) // Pattern cannot be repeated twice in a 1-digit number.
                 continue;
 
-            int half = length / 2;
-            if (digits[..half] != digits[half..])
+            // Find all divisions of the number as a string.
+            var factors = new HashSet<int>();
+            for (var x = 1; x <= length / 2; x++)
+                if (length % x == 0)
+                    factors.Add(x);
+
+            // Through each possible division, check if all substrings match.
+            var valid = true;
+            foreach (int factor in factors)
+            {
+                string compareString = digits[..factor];
+
+                var match = true;
+                int skip = factor;
+                while (skip <= length - factor)
+                {
+                    if (digits.Substring(skip, factor) != compareString)
+                    {
+                        // If the substring doesn't match, the pattern appears valid from this possible division.
+                        match = false;
+                        break;
+                    }
+                    skip += factor;
+                }
+
+                if (!match)
+                    continue;
+
+                valid = false;
+                break;
+            }
+
+            if (valid)
                 continue;
 
             invalidIds.Add(i);
--- Tests/Day2.cs
+++ Tests/Day2.cs
@@ -35,6 +35,7 @@ public class Day2
     }
 
     [Test]
+    [Skip("Invalid for Part 2")]
     public async Task Part1ExampleProducesExpectedOutput()
     {
         // Arrange
@@ -45,6 +46,30 @@ public class Day2
         );
 
         const ulong expectedOutput = 1227775554;
+        var validator = new IdValidator();
+
+        // Act
+        foreach ((uint lower, uint upper) in inputs)
+        {
+            var ids = validator.FindInvalidIds(lower, upper);
+            Console.WriteLine($"Found {ids.Count} invalid IDs in range {lower}-{upper}: {string.Join(", ", ids)}");
+        }
+
+        // Assert
+        await Assert.That(validator.Sum).IsEqualTo(expectedOutput);
+    }
+
+    [Test]
+    public async Task Part2ExampleProducesExpectedOutput()
+    {
+        // Arrange
+        var inputs = InputParser.Parse(
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,"     +
+            "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+            "824824821-824824827,2121212118-2121212124"
+        );
+
+        const ulong expectedOutput = 4174379265;
         var validator = new IdValidator();
 
         // Act
